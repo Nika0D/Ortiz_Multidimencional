9b550cf98824e118e422418cac12eb3d23136ee6
Nicolay Ortiz <nicolay.ortiz@udla.edu.ec>
2023-11-30 19:54:59 -0500

    fin

--- Problema_matrices.c
+++ Problema_matrices.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 int main(){
     int y, x, z;
+    /*buscamos relizar matrices del tamaño y cantidades que el usuario 
+    desee y colocar todos los valores en 0 excepto la ultima que sera 1*/
     printf("Escriba un tamaño de filas\n");
     scanf("%d", &y);
     printf("Escriba un tamaño de columnas\n");
     scanf("%d", &x);
     printf("Escriba la cantidad de matrices\n");
     scanf("%d", &z);
+    // inicializo la matris es 3 coordenadas y añado un for externo para cambiar en el eje z
     int matA[z][y][x];
     for (int k = 0; k < z; k++)
     {
@@ -14,6 +17,7 @@ int main(){
         {
             for (int j = 0; j < x; j++)
             {
+                //la condicion clave oara que solo la ultima sea 1
                 if (k!=z-1)
                 {
                     matA[k][i][j]=0;
